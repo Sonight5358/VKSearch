1e59fb829e2d8dc732e2b0c4e0c92aad97d8e24a
Sonight5358 <Dvoyaga19@gmail.com>
2026-07-10 22:39:50 +0300

    Add httplib and nlohmann

---
+++ VKSearchServer/VKSearchServer.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+#include "httplib.h"
+#include "nlohmann/json.hpp"
+
+using json = nlohmann::json;
+
+int main()
+{
+    httplib::Client cli("localhost", 9200);
+
+    auto res = cli.Get("/");
+
+    if (res)
+    {
+        std::cout << "Status: " << res->status << '\n';
+        std::cout << "Body: " << res->body << '\n';
+
+        json parsed = json::parse(res->body);
+        std::cout << "Cluster name: " << parsed["cluster_name"] << '\n';
+    }
+    else
+    {
+        std::cout << "Request failed: " << httplib::to_string(res.error()) << '\n';
+    }
+
+    return 0;
+}
